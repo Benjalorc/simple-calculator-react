efdfd836ca2f5f4dc6ae86bd3da8deedf101602a
Benjalorc <benjalorc@gmail.com>
2020-06-08 09:12:00 -0400

    Cambio de tipo de componente Calculator a clase Eventos de botones para entrada de numeros Botones de numeros renderizados como map

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, {createRef} from 'react';
 import './Calculator.css';
 
-function Calculator() {
-  return (
-    <div className="calculator">
-      <input type="number" />
-      <div className="left-area">
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button">,</button>
-        <button type="button">0</button>
-        <button type="button">=</button>
+class Calculator extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.numberInput = createRef(0);
+    this.lastInput = createRef();
+    this.numeros = [
+      "7", "8", "9",
+      "4", "5", "6",
+      "1", "2", "3",
+      ".", "0",
+    ]
+    this.addToLast = this.addToLast.bind(this);
+  }
+
+  addToLast(e){
+    let value = e.target.value;
+    if(this.lastInput.current === "."){
+      value = this.numberInput.current.value.length === 0 ? ("0."+value) : ("."+value);
+    }
+    else if(value === "."){
+      value = "";
+    }
+    this.numberInput.current.value += value;
+    this.lastInput.current = e.target.value;
+  }
+
+  render(){
+    return (
+      <div className="calculator">
+        <input ref={this.numberInput} type="number" />
+        <div className="left-area">
+          {
+            this.numeros.map((num, index)=>{
+              return <button type="button" key={num} value={num} onClick={this.addToLast}>{num}</button>
+            })
+          }
+          <button type="button">=</button>
+        </div>
+        <div className="right-area">
+          <button type="button">CE</button>
+          <button type="button">/</button>
+          <button type="button">*</button>
+          <button type="button">-</button>
+          <button type="button">+</button>
+        </div>
       </div>
-      <div className="right-area">
-        <button type="button">CE</button>
-        <button type="button">/</button>
-        <button type="button">*</button>
-        <button type="button">-</button>
-        <button type="button">+</button>
-      </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default Calculator;
